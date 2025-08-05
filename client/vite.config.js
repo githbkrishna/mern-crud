961cb2c17c4c126745b4b9244a3ec186aae2df88
githbkrishna <krishna636471@gmail.com>
2025-08-05 16:39:50 +0530

    connected frontend to backend after disableling the ssl certificate

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tls from 'tls';
+
+// Ignore SSL cert verification in dev (ONLY)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +13,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:5000', // Backend server URL
-        target: 'https://mern-crud-app-1.netlify.app', // Backend server URL
+        target: 'https://mern-crud-psjs.onrender.com', // Backend server URL
+        changeOrigin: true,
+        secure: false, // <-- This disables SSL cert verification
       },
     },
   },
